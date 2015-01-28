9a0875f90a6028bc24b17925fc518f2b8223c564
yuanzm <1229084233@qq.com>
2015-01-28 21:26:28 +0800

    add version0.0.1

--- HolaWith/routes/index.js
+++ HolaWith/routes/index.js
@@ -35,6 +35,24 @@ var data = {
     ]
 }
 
+
+test = {
+  "data": [
+    {
+      "name": "百事可乐",
+      "license": "12330393",
+      "email": "1229084233@qq.com",
+      "status": "已通过",
+    },
+    {
+      "name": "百事可乐",
+      "license": "12330393",
+      "email": "1229084233@qq.com",
+      "status": "已通过",
+    }
+  ]
+}
+
 review = {
     "data": [
       {
@@ -42,35 +60,35 @@ review = {
         "license": "12330393",
         "id": "214234",
         "email": "1229084233@qq.com",
-        "status": "已通过",
+        "status": "待审核",
       },
       {
         "name": "可口可乐",
-        "license": "12330393",
+        "license": "12330394",
         "id": "214234",
         "email": "1229084233@qq.com",
-        "status": "已通过",
+        "status": "待审核",
       },
       {
         "name": "百事可乐",
-        "license": "12330393",
+        "license": "12330395",
         "id": "214234",
         "email": "1229084233@qq.com",
-        "status": "已通过",
+        "status": "待审核",
       },
       {
         "name": "百事可乐",
-        "license": "12330393",
+        "license": "12330396",
         "id": "214234",
         "email": "1229084233@qq.com",
-        "status": "已通过",
+        "status": "待审核",
       },
       {
         "name": "百事可乐",
-        "license": "12330393",
+        "license": "12330397",
         "id": "214234",
         "email": "1229084233@qq.com",
-        "status": "已通过",
+        "status": "待审核",
       }
       ]
 }
@@ -103,6 +121,24 @@ router.get('/review', function(req, res) {
 
 router.get('/allreview', function(req, res) {
   res.json(review);
+})
+
+router.post('/:id/accept', function(req, res) {
+    id = req.body.id;
+    res.send('success');
+});
+
+router.post('/:id/refuse', function(req, res) {
+    id = req.body.id;
+    res.send('success');
+});
+
+router.get('/raider', function(req, res) {
+  res.render("raiders-release");
+})
+
+router.get('/raider-detail', function(req, res) {
+  res.render("raiders-detail");
 })
 
 module.exports = router;
